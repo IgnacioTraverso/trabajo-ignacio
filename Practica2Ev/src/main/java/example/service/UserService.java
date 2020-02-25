697bf96db406dafc15d2f43708bb9627293a1ff3
Ignacio <49472831+IgnacioTraverso@users.noreply.github.com>
2020-02-25 02:01:23 +0100

    Update UserService.java

--- Practica2Ev/src/main/java/example/service/UserService.java
+++ Practica2Ev/src/main/java/example/service/UserService.java
@@ -7,12 +7,6 @@ import example.model.UserModel;
 public interface UserService {
 	public abstract User model2entity(UserModel userModel);
 	public abstract UserModel entity2model(User user);
-
-	public abstract List<UserModel> listAllUser();
-	public abstract User addUser(UserModel userModel);
-	public abstract boolean checkUser(String username);
-	
-	public abstract void save(UserModel userModel);
-	
-	
+	public abstract List<User> getListUsuarios();
+	public abstract User addUsuario(UserModel userModel);
 }
