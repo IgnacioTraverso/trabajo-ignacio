bb9c3652017dafac1d1d24f6ec47abec5dcf743c
Ignacio <49472831+IgnacioTraverso@users.noreply.github.com>
2020-02-25 01:59:30 +0100

    Update RegistroController.java

--- Practica2Ev/src/main/java/example/controller/view/RegistroController.java
+++ Practica2Ev/src/main/java/example/controller/view/RegistroController.java
@@ -1,26 +1,26 @@
 package example.controller.view;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import example.constantes.Constantes;
 import example.crypt.TestCrypt;
+import example.entity.User;
 import example.model.UserModel;
 import example.model.UserRoleModel;
-import example.service.UserRoleService;
-import example.service.UserService;
-import example.validator.RegistroValidator;
+import example.service.impl.UserRoleServiceImpl;
+import example.service.impl.UserServiceImpl;
 
 
 @Controller
@@ -29,43 +29,31 @@ public class RegistroController {
 	
 	@Autowired
 	@Qualifier("UserServiceImpl")
-	private UserService userService;
+	private UserServiceImpl userServiceImpl;
 	
 	@Autowired
 	@Qualifier("userRoleServiceImpl")
-	private UserRoleService userRoleService;
+	private UserRoleServiceImpl userRoleImpl;
 	
-	private TestCrypt encrypt=new TestCrypt();
-	
-	private RegistroValidator validate=new RegistroValidator();
-	private static final Log LOGGER = LogFactory.getLog(CiclistaController.class);
+	private TestCrypt enc=new TestCrypt();
 
+	
 	@GetMapping("/registro")
-	public String registrar(Model model) {
-		UserModel userModel=new UserModel();
-		model.addAttribute("user",userModel);
-		return Constantes.REGISTRO_VIEW;
+	public ModelAndView registrar() {
+		ModelAndView mav =new ModelAndView(Constantes.REGISTRO_VIEW);
+		mav.addObject("user",new User());
+		return mav;
 	}
 	
 	@PostMapping("/addUser")
-	public ModelAndView addUser(@ModelAttribute("user") UserModel userModel,UserRoleModel userRoleModel, Model model, BindingResult result, RedirectAttributes flash) {
-		ModelAndView mav=new ModelAndView();
-		LOGGER.info(userModel.getUsername());
-		this.validate.validate(userModel, result);
-		if(result.hasErrors() || userService.checkUser(userModel.getUsername())) {
-			if(userService.checkUser(userModel.getUsername())) {
-				model.addAttribute("error",1);
-			}
-			mav.setViewName(Constantes.REGISTRO_VIEW);
-		}else {
-			userModel.setPassword(encrypt.generatepassword(userModel.getPassword()));
-			userModel.setEnabled(true);
-			userService.save(userModel);
-			userRoleModel.setUser(userModel);
-			userRoleService.addUserRole(userRoleModel);
-			mav.setViewName("redirect:/practica/login");
-			flash.addFlashAttribute("success","User registrado");
-		}
+	public ModelAndView addUser(@ModelAttribute("user") UserModel userModel,@RequestParam(name="patata")  String rol, UserRoleModel userRoleModel, Model model, RedirectAttributes flash) {
+		ModelAndView mav = new ModelAndView();
+		userModel.setPassword(enc.generatepassword(userModel.getPassword()));
+		userServiceImpl.addUser(userModel);
+		userRoleModel.setRole(rol);
+		userRoleModel.setUser(userModel);
+		userRoleImpl.addUsuario(userRoleModel);
+		mav.setViewName("redirect:/practica/login");
 		return mav;
 	}
 }
