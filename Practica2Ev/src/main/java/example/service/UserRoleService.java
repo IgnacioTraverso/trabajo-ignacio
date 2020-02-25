d21d8f38faa22d7bd1696a758746d8ec527ee6c7
Ignacio <49472831+IgnacioTraverso@users.noreply.github.com>
2020-02-25 02:00:41 +0100

    Update UserRoleService.java

--- Practica2Ev/src/main/java/example/service/UserRoleService.java
+++ Practica2Ev/src/main/java/example/service/UserRoleService.java
@@ -1,11 +1,15 @@
 package example.service;
 
 
+import java.util.List;
+
 import example.entity.UserRole;
 import example.model.UserRoleModel;
 
 public interface UserRoleService {
+	
 	public abstract UserRole model2entity(UserRoleModel userRoleModel);
 	public abstract UserRoleModel entity2model(UserRole userRole);
-	public abstract UserRole addUserRole(UserRoleModel userRoleModel);
+	public abstract List<UserRole> getListUsuarios();
+	public abstract UserRole addUsuario(UserRoleModel userRoleModel);
 }
