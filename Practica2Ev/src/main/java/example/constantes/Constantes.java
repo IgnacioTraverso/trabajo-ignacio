f4bbeb0d9c4fc22cba58462decc31b8998373206
Ignacio <49472831+IgnacioTraverso@users.noreply.github.com>
2020-02-25 02:16:34 +0100

    Update Constantes.java

--- Practica2Ev/src/main/java/example/constantes/Constantes.java
+++ Practica2Ev/src/main/java/example/constantes/Constantes.java
@@ -4,14 +4,13 @@ public class Constantes {
 	public static final String LOGIN_VIEW="Inicio/login";	
 	public static final String REGISTRO_VIEW="registro";	
 	public static final String INICIO_VIEW="inicio";	
-	public static final String EQUIPO_VIEW="read/equipos";		
-	public static final String CICLISTAS_VIEW="read/ciclistas";
-	public static final String MAILLOT_VIEW="read/maillots";	
-	public static final String ETAPA_VIEW="read/etapas";	
-	public static final String PUERTO_VIEW="read/puertos";
-	public static final String LLEVARS_VIEW="read/llevars";
-	public static final String DETALLESEQUIPO_VIEW="read/detalleEquipo";
-	public static final String DETALLESCICLISTA_VIEW="read/detalleCiclista";
+	public static final String EQUIPO_VIEW="Read/equipos";		
+	public static final String CICLISTAS_VIEW="Read/ciclistas";
+	public static final String MAILLOT_VIEW="Read/maillots";	
+	public static final String ETAPA_VIEW="Read/etapas";	
+	public static final String PUERTO_VIEW="Read/puertos";
+	public static final String LLEVARS_VIEW="Read/llevars";
+	
 	public static final String APUNTAR_VIEW="Forms/apuntar";
 	public static final String EQUIPO="Forms/createequipo";	
 	public static final String MAILLOT="Forms/createmaillot";
